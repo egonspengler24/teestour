259f04b13ef56fe444fa7091f32ac8cfa71d6339
egonspengler24 <98477110+egonspengler24@users.noreply.github.com>
2022-01-26 16:12:23 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010123jpg",
+      "name": "R0010123.JPG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.5820003028711263,
+        "pitch": -0.06115632059456999,
+        "fov": 1.5656018072211058
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3993313740891953,
+          "pitch": 0.16251858763926208,
+          "rotation": 0,
+          "target": "1-r0010221"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.38425106595573233,
+          "pitch": 0.11073024383162888,
+          "title": "This is a box",
+          "text": "And this is a description of the box"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010221",
+      "name": "R0010221",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
